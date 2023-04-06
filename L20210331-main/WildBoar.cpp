36edf8434d0a1194c03a681375ff1531dbdd26b8
Sho1007 <sho1007@kookmin.ac.kr>
2023-04-06 09:42:05 +0900

    Create WildBoar Class Inherited Monster

---
+++ L20210331-main/WildBoar.cpp
@@ -0,0 +1,6 @@
+#include "WildBoar.h"
+
+WildBoar::WildBoar(int _PositionY, int _PositionX, float _HP, float _MP)
+	: Monster("WildBoar", _PositionY, _PositionX, _HP, _MP)
+{
+}
---
+++ L20210331-main/WildBoar.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Monster.h"
+class WildBoar : public Monster
+{
+public:
+	WildBoar(int _PositionY, int _PositionX, float _HP = 100.0f, float _MP = 100.0f);
+};
+
